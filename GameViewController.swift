3e8befe7a83dffa177d01847398d1526f8857b21
olgagian <ioannis@poleiseis.giannakidis.local>
2017-01-27 17:48:12 +0200

    inactions

---
+++ GameViewController.swift
@@ -0,0 +1,37 @@
+//
+//  GameViewController.swift
+//  GameCollector
+//
+//  Created by ioannis giannakidis on 27/01/2017.
+//  Copyright © 2017 ioannis giannakidis. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var gameImageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+  
+    @IBAction func photosTapped(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        
+        
+    }
+    @IBAction func addTapped(_ sender: Any) {
+        
+    }
+    
+}
