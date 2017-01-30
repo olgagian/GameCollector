57a588f570dc357b5c177e59ce86fb9b9224cb15
olgagian <ioannis@poleiseis.giannakidis.local>
2017-01-30 10:39:24 +0200

    change 2

--- GameViewController.swift
+++ GameViewController.swift
@@ -8,21 +8,26 @@
 
 import UIKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     @IBOutlet weak var gameImageView: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
+    var imagePicker = UIImagePickerController()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
+        
 
         // Do any additional setup after loading the view.
     }
 
   
     @IBAction func photosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
         
+        present(imagePicker, animated: true, completion: nil)
         
     }
     
